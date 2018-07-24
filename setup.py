b50b4efca4c90e86e6b8a6fc8967c279862946e2
YingLi Lu <yinglilu@gmail.com>
2018-07-24 11:36:52 -0400

    update setup.py: version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "0.1"
+VERSION = "0.1.1"
 setuptools.setup(name='extractCMRRPhysio',
                  version=VERSION,
                  description='extractCMRRPhysio.m(https://github.com/CMRR-C2P/MB/blob/master/extractCMRRPhysio.m) in Python',
